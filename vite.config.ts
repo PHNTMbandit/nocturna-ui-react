2676a9f60b35eb024b6032847a56ad2661052435
Domenic Pittari <dom.pittari@gmail.com>
2025-12-26 12:09:14 +0900

    feat: move components to index formation

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import pkg from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    dts({
+      entryRoot: "src",
+      outDir: "dist",
+      insertTypesEntry: true,
+      rollupTypes: true,
+    }),
+    tailwindcss(),
   ],
-})
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
+      name: "orbis-ui-react",
+      fileName: (format) => `orbis-ui-react.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+    },
+    rollupOptions: {
+      external: Object.keys(pkg.peerDependencies),
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
