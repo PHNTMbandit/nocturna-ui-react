c4e6abc399bb50a895248908bf410bb2381e4a87
Domenic Pittari <dom.pittari@gmail.com>
2026-01-04 10:43:32 +0900

    feat: add tw-animate-css dependency and update theme switching in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import type * as React from "react"
 import { cn } from "@/utils/cn"
-import { Button, Switch } from "./components"
+import { Button } from "./components"
 
 type AppProps = React.ComponentProps<"div">
 
 export const App = ({ className, children, ref, ...props }: AppProps) => {
+	const setTheme = (theme: string) => {
+		const html = document.documentElement
+		document.documentElement.removeAttribute("data-theme")
+
+		if (theme === "dark") {
+			html.setAttribute("data-theme", "dark")
+		}
+		if (theme === "light") {
+			html.setAttribute("data-theme", "light")
+		}
+	}
+
 	return (
-		<div className={cn("", className)} ref={ref} {...props}>
+		<div className={cn("bg-primary", className)} ref={ref} {...props}>
 			{children}
-			<Button>Click Me</Button>
-			<Button className="text-on-primary" tone={"error"}>
-				Click Me
+			<Button
+				onClick={() => {
+					setTheme("light")
+				}}
+			>
+				Light
 			</Button>
-			<Switch />
+			<Button
+				className="text-on-primary"
+				onClick={() => setTheme("dark")}
+				tone={"error"}
+			>
+				Dark
+			</Button>
 		</div>
 	)
 }
