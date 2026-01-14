062d759dc7f400e114f4b85cb9b44fa2265250ac
Domenic Pittari <dom.pittari@gmail.com>
2026-01-14 11:50:23 +0900

    feat: add navigation menu ui component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Badge, Button, Slider, ToastProvider } from "./components"
 import "./styles.css"
 
 const rootElement = document.getElementById("root")
@@ -8,15 +7,7 @@ const rootElement = document.getElementById("root")
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			<ToastProvider>
-				<main>
-					<Button>Click me</Button>
-					<Slider />
-					<Badge radius="circle" tone="primary">
-						New
-					</Badge>
-				</main>
-			</ToastProvider>
+			<main className=""></main>
 		</React.StrictMode>,
 	)
 }
