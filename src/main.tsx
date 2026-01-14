512ae7cc1e849a1bfa2e2f275b437cac862493bd
Domenic Pittari <dom.pittari@gmail.com>
2026-01-14 12:23:14 +0900

    feat(input): add suffix to inputs
    n

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./styles.css"
+import { CommandIcon, MagnifyingGlassIcon } from "@phosphor-icons/react"
+import { Badge, Input } from "./components"
 
 const rootElement = document.getElementById("root")
 
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			<main className=""></main>
+			<main className="p-2xl">
+				<Input leadingIcon={MagnifyingGlassIcon} placeholder="Testing now...">
+					<Badge>
+						<CommandIcon weight="bold" />K
+					</Badge>
+				</Input>
+			</main>
 		</React.StrictMode>,
 	)
 }
